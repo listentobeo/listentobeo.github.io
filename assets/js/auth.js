508ed688c03804c742e4b06203b61043afda70f1
listentobeo <odekeb9@gmail.com>
2026-03-13 23:19:49 +0100

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -2,10 +2,45 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js"
 
 const supabase = createClient(
 "https://wphqcccliiwdvwdjgrmc.supabase.co",
-"sb_publishable_-VkVZ5mPWa3EPEqHCmE3dw_UvOZBiXo"
+"YOUR_PUBLISHABLE_KEY"
 )
 
-async function getUser() {
-const { data } = await supabase.auth.getUser()
-return data.user
+async function loginUser(){
+
+const email = document.getElementById("email").value
+const password = document.getElementById("password").value
+
+const { error } = await supabase.auth.signInWithPassword({
+email: email,
+password: password
+})
+
+if(error){
+alert(error.message)
+return
+}
+
+alert("Login successful")
+
+window.location.href = "/"
+
+}
+
+async function signupUser(){
+
+const email = document.getElementById("email").value
+const password = document.getElementById("password").value
+
+const { error } = await supabase.auth.signUp({
+email: email,
+password: password
+})
+
+if(error){
+alert(error.message)
+return
+}
+
+alert("Account created. Please login.")
+
 }
