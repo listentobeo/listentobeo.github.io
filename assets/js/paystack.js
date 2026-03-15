7b4b68676671559b0d2f5f286c4174080014d992
listentobeo <odekeb9@gmail.com>
2026-03-15 13:21:37 +0100

    Update paystack.js

--- assets/js/paystack.js
+++ assets/js/paystack.js
@@ -1,7 +1,7 @@
 function buyCredits(email, amount){
 
 let handler = PaystackPop.setup({
-key: "pk_live_2d3dff6f69fb6093c1df177517a802e0de68a731",
+key: "PAYSTACK_API_KEY",
 email: email,
 amount: amount * 100,
 currency: "NGN",
